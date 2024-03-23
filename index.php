9c49b77960eac9567bb436c1a1181df6076cdfb5
Kamal Zakoune <kmldeveloper@gmail.com>
2024-03-23 17:51:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 // Replace these with your actual app credentials
 $clientId = '944463346552351';
 $clientSecret = '5054166e9895b4e93359d385965e126e';
-$redirectUri = 'https://winkmentions-0e9019ab59d4.herokuapp.com/';
+$redirectUri = 'https://winkmentions-0e9019ab59d4.herokuapp.com/fronend.html';
 
 // Check if user is already authenticated
 if (isset($_SESSION['access_token'])) {
